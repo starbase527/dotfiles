b39e47d0703c40e5edf52293fa3f0a2ac6b4ccea
starbase527 <starbase527@users.noreply.github.com>
2017-06-13 16:13:51 +1000

    Added submodule initialisation and ZDOTDIR setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+BASEDIR="${0:A:h}";
+cd $BASEDIR;
+git submodule update --init --recursive "dotbot";
+
+if [[ `uname` = 'Darwin' ]]; then
+
 # Setup minimal Dock
 if $(which dockutil > /dev/null); then
 	local uni_folder="$HOME/Documents/University/";
@@ -18,5 +24,21 @@ if $(which dockutil > /dev/null); then
 		--view grid --display folder --sort kind --after "$uni_folder";
 	dockutil --add "$HOME/Downloads" \
 		--view grid --display folder --sort datemodified --after "`whoami`";
+fi;
+
+fi;
+
+# This part will need root permissions
+local ZSHENVFILE;
+if [ -d /etc/zsh/zshenv ]; then
+	ZSHENVFILE=/etc/zsh/zshenv;
+else
+	ZSHENVFILE=/etc/zshenv;
+fi;
+
+if [[ ! $(grep 'ZDOTDIR' $ZSHENVFILE) ]]; then
+	sudo echo '$ZDOTDIR=$HOME/.config/zsh' >> $ZSHENVFILE;
+else
+	echo "You need to update zsh to respect ~/.config/zsh manually"
 fi;
 
