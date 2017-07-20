f556f88f55145e494970f12940927eb5c202a56c
starbase527 <starbase527@users.noreply.github.com>
2017-07-20 17:45:24 +1000

    Added comments and descriptive outputs to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ cd $BASEDIR;
 git submodule update --init --recursive "dotbot";
 
 if [[ `uname` = 'Darwin' ]]; then
+echo "Beginning macOS-specific configuration...";
 
 # Setup minimal Dock
 if $(which dockutil > /dev/null); then
+	echo -n "\tSetting up dock... ";
 	local uni_folder="$HOME/Documents/University/";
 	uni_folder=$uni_folder/`ls $uni_folder | tail -n 1`;
 
@@ -24,11 +26,15 @@ if $(which dockutil > /dev/null); then
 		--view grid --display folder --sort kind --after "$uni_folder";
 	dockutil --add "$HOME/Downloads" \
 		--view grid --display folder --sort datemodified --after "`whoami`";
+	echo "done.";
 fi;
 
+echo "done macOS-specific configuration.";
 fi;
 
+# Make zsh put its configuration files in ~/.config
 # This part will need root permissions
+echo -n "Updating zsh to use ~/.config... "
 local ZSHENVFILE;
 if [ -d /etc/zsh ]; then
 	ZSHENVFILE=/etc/zsh/zshenv;
@@ -36,10 +42,12 @@ else
 	ZSHENVFILE=/etc/zshenv;
 fi;
 
-if [[ ! $(grep 'ZDOTDIR' $ZSHENVFILE) ]]; then
-	sudo echo 'ZDOTDIR="${XDG_CONFIG_HOME:-$HOME/.config/zsh}"' \
-		>> $ZSHENVFILE;
+if [[ ! $(grep 'ZDOTDIR' $ZSHENVFILE &> /dev/null) ]]; then
+	echo 'ZDOTDIR="${XDG_CONFIG_HOME:-$HOME/.config/zsh}"' \
+		| sudo tee -a $ZSHENVFILE >> /dev/null;
+	echo "done.";
 else
-	echo "You need to update zsh to respect ~/.config/zsh manually"
+	echo;
+	echo "You may need to update zsh to respect ~/.config/zsh manually";
 fi;
 
