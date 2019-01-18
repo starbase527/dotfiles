d131006f72ef1a09f8dfacfaa5b0586503244878
starbase527 <starbase527@users.noreply.github.com>
2019-01-19 02:50:03 +1100

    mosh: mosh-alias.sh no longer assumes server

--- local/bin/mosh-alias.sh
+++ local/bin/mosh-alias.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-$(ssh newcross.lan 'echo $SSH_CONNECTION; mosh-server' \
+$(ssh $1 'echo $SSH_CONNECTION; mosh-server' \
 	|& head -n 2 \
 	| awk \
 	'/22/{ip=$3}; /CONNECT/{print "eval MOSH_KEY="$4,"mosh-client "ip,$3,$5}')
