54e9c7e3fa3dffa23bec40b536ed79e8c45f6eed
starbase527 <starbase527@users.noreply.github.com>
2019-12-06 14:39:53 +1100

    osx-setup: No longer fiddles with Spotlight

--- osx-setup.sh
+++ osx-setup.sh
@@ -138,31 +138,6 @@ open -ag Terminal "terminal-solarized/Solarized Light.terminal"
 defaults write com.apple.Terminal "Default Window Settings" "Solarized Dark"
 defaults write com.apple.Terminal "Startup Window Settings" "Solarized Dark"
 
-# Spotlight
-defaults write com.apple.Spotlight orderedItems -array \
-	'{"enabled" = 1;"name" = "MENU_CONVERSION";}' \
-	'{"enabled" = 1;"name" = "MENU_DEFINITION";}' \
-	'{"enabled" = 1;"name" = "MENU_WEBSEARCH";}' \
-	'{"enabled" = 1;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}' \
-	'{"enabled" = 0;"name" = "APPLICATIONS";}' \
-	'{"enabled" = 0;"name" = "SYSTEM_PREFS";}' \
-	'{"enabled" = 0;"name" = "DIRECTORIES";}' \
-	'{"enabled" = 0;"name" = "PDF";}' \
-	'{"enabled" = 0;"name" = "FONTS";}' \
-	'{"enabled" = 0;"name" = "DOCUMENTS";}' \
-	'{"enabled" = 0;"name" = "MESSAGES";}' \
-	'{"enabled" = 0;"name" = "CONTACT";}' \
-	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
-	'{"enabled" = 0;"name" = "IMAGES";}' \
-	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
-	'{"enabled" = 0;"name" = "MUSIC";}' \
-	'{"enabled" = 0;"name" = "MOVIES";}' \
-	'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-	'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-	'{"enabled" = 0;"name" = "SOURCE";}' \
-	'{"enabled" = 0;"name" = "MENU_OTHER";}' \
-	'{"enabled" = 0;"name" = "MENU_EXPRESSION";}'
-
 # TODO: Mac App Store
 
 set +o XTRACE
